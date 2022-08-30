48d458d89baad689ef0986fd85670a2e289007a7
森田陸斗 <mrmrt1121@gmail.com>
2022-08-30 23:34:50 +0900

    loggerでAPI通信を確認

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:github_repo_search/utils/logger.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'app.dart';
 
 void main() {
   Logger.configure();
 
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
