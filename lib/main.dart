ccf78c5c13463994a0e2380396bfb5e8875d539b
森田陸斗 <mrmrt1121@gmail.com>
2022-08-30 18:27:07 +0900

    Loggerを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+// Flutter imports:
 import 'package:flutter/material.dart';
 
+// Project imports:
+import 'package:github_repo_search/utils/logger.dart';
 import 'app.dart';
 
 void main() {
+  Logger.configure();
+
   runApp(const App());
 }
