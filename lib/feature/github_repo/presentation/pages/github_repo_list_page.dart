d2f2f91ce979950ee83862c851d28a95783435cd
森田陸斗 <mrmrt1121@gmail.com>
2022-09-01 15:56:48 +0900

    検索キーワードを検索プロバイダーで監視

--- lib/feature/github_repo/presentation/pages/github_repo_list_page.dart
+++ lib/feature/github_repo/presentation/pages/github_repo_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_repo_search/feature/github_repo/presentation/widgets/search_form.dart';
 import 'package:github_repo_search/feature/github_repo/provider/repo_search_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,28 +14,35 @@ class GithubRepoListPage extends ConsumerWidget {
         title: const Text('GitHub'),
       ),
       body: SafeArea(
-        child: githubRepos.when(
-          data: (data) {
-            final repositories = data.items;
-            return ListView.separated(
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(repositories[index].fullName),
-                  subtitle: Text(repositories[index].description),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const Divider();
-              },
-              itemCount: data.items.length,
-            );
-          },
-          error: (error, stack) => Center(
-            child: Text(error.toString()),
-          ),
-          loading: () => const Center(
-            child: CircularProgressIndicator.adaptive(),
-          ),
+        child: Column(
+          children: [
+            const SearchFrom(),
+            Expanded(
+              child: githubRepos.when(
+                data: (data) {
+                  final repositories = data.items;
+                  return ListView.separated(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(repositories[index].fullName),
+                        subtitle: Text(repositories[index].description),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Divider();
+                    },
+                    itemCount: data.items.length,
+                  );
+                },
+                error: (error, stack) => Center(
+                  child: Text(error.toString()),
+                ),
+                loading: () => const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
