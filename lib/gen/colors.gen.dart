200c1114a60df73bf2c639f906513f5897f5aa07
森田陸斗 <mrmrt1121@gmail.com>
2022-09-02 17:30:01 +0900

    色をflutter_genで追加

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,45 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #282c2c
+  static const Color backgroundDark = Color(0xFF282C2C);
+
+  /// Color: #e5eaee
+  static const Color backgroundLight = Color(0xFFE5EAEE);
+
+  /// Color: #8250de
+  static const Color primary = Color(0xFF8250DE);
+
+  /// Color: #161b21
+  static const Color primaryDark = Color(0xFF161B21);
+
+  /// Color: #444444
+  static const Color textColor = Color(0xFF444444);
+
+  /// Color: #e6e6e6
+  static const Color textColorDark = Color(0xFFE6E6E6);
+
+  /// Color: #dedede
+  static const Color textFieldColor = Color(0xFFDEDEDE);
+
+  /// Color: #1e231e
+  static const Color textFieldColorDark = Color(0xFF1E231E);
+
+  /// Color: #777777
+  static const Color textSubColor = Color(0xFF777777);
+
+  /// Color: #c0c0c0
+  static const Color textSubColorDark = Color(0xFFC0C0C0);
+}
