175496731e7efc15dd1660e0669e7495b1c2b409
森田陸斗 <mrmrt1121@gmail.com>
2022-09-02 14:13:50 +0900

    RepoTileをInkWellWidgetでラップ

--- lib/feature/github_repo/presentation/widgets/repo_tile.dart
+++ lib/feature/github_repo/presentation/widgets/repo_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:github_repo_search/feature/github_repo/model/github_repo.dart';
+import 'package:github_repo_search/utils/logger.dart';
 
 class RepoTile extends StatelessWidget {
   const RepoTile({super.key, required this.repository});
@@ -9,21 +10,26 @@ class RepoTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            repository.fullName,
-            style: Theme.of(context).textTheme.bodyMedium,
-            overflow: TextOverflow.ellipsis,
-          ),
-          Text(
-            repository.description,
-            style: Theme.of(context).textTheme.bodySmall,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ],
+      child: InkWell(
+        onTap: () {
+          logger.info(repository.fullName);
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              repository.fullName,
+              style: Theme.of(context).textTheme.bodyMedium,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              repository.description,
+              style: Theme.of(context).textTheme.bodySmall,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
       ),
     );
   }
