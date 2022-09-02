11e7b629be1fd99a2c9b44f83e506686295fa07f
森田陸斗 <mrmrt1121@gmail.com>
2022-09-02 14:30:36 +0900

    リポジトリ詳細画面の作成

---
+++ lib/feature/github_repo/presentation/pages/github_repo_detail_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:github_repo_search/feature/github_repo/model/github_repo.dart';
+
+class GithubRepoDetailPage extends StatelessWidget {
+  const GithubRepoDetailPage({super.key, required this.repository});
+
+  final GithubRepo repository;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          repository.fullName,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(repository.owner.avatarUrl),
+                    ),
+                    const Gap(10),
+                    Text(repository.owner.login),
+                  ],
+                ),
+                Text(
+                  repository.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(fontSize: 20),
+                ),
+                const Gap(10),
+                Text(
+                  repository.description,
+                ),
+                _CustomTile(
+                  icon: Icons.language,
+                  title: 'language',
+                  iconColor: Colors.blue,
+                  child: repository.language != null
+                      ? Text(repository.language!)
+                      : const Text('-'),
+                ),
+                _CustomTile(
+                  icon: Icons.star_border,
+                  title: 'star',
+                  iconColor: const Color(0xFFedb918),
+                  child: Text('${repository.stargazersCount} stars'),
+                ),
+                _CustomTile(
+                  icon: Icons.remove_red_eye_outlined,
+                  title: 'watch',
+                  iconColor: Colors.red,
+                  child: Text('${repository.watchersCount} watching'),
+                ),
+                _CustomTile(
+                  icon: Icons.fork_left,
+                  title: 'fork',
+                  iconColor: Colors.grey,
+                  child: Text('${repository.forksCount} forks'),
+                ),
+                _CustomTile(
+                  icon: Icons.adjust,
+                  title: 'issue',
+                  iconColor: Colors.green,
+                  child: Text('${repository.openIssuesCount} issues'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomTile extends StatelessWidget {
+  const _CustomTile({
+    required this.iconColor,
+    required this.icon,
+    required this.title,
+    required this.child,
+  });
+
+  final Color iconColor;
+  final IconData icon;
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(
+            icon,
+            color: iconColor,
+          ),
+          title: Text(title),
+          trailing: child,
+        ),
+        const Divider(),
+      ],
+    );
+  }
+}
