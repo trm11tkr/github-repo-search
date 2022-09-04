f1aa246bd857afe840491c2a3acca65dbc772b6c
森田陸斗 <mrmrt1121@gmail.com>
2022-09-04 13:01:35 +0900

    resizeToAvoidBottomInsetをfalseに変更

--- lib/feature/github_repo/presentation/pages/github_repo_list_page.dart
+++ lib/feature/github_repo/presentation/pages/github_repo_list_page.dart
@@ -8,6 +8,7 @@ class GithubRepoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('GitHub'),
       ),
