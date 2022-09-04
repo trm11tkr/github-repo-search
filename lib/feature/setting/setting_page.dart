09084743998a1ea543856f5af36c8dd52030e40d
森田陸斗 <mrmrt1121@gmail.com>
2022-09-04 15:22:12 +0900

    SettinPageでThemeを変更できる機能の追加

--- lib/feature/setting/setting_page.dart
+++ lib/feature/setting/setting_page.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:github_repo_search/core/extension/theme_mode_extension.dart';
+import 'package:github_repo_search/feature/setting/theme_controller.dart';
+import 'package:github_repo_search/gen/colors.gen.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends ConsumerWidget {
   const SettingPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // <StateNotifier>ThemeSelectorのインスタンスを監視
+    final themeSelector = ref.watch(themeSelectorProvider.notifier);
+    // 現在選択されているThemeModeを監視
+    final currentThemeMode = ref.watch(themeSelectorProvider);
     return Scaffold(
-      appBar: AppBar(title: const Text('設定')),
-      body: const Center(
-        child: Text('設定画面'),
+      appBar: AppBar(
+        title: const Text('ThemeMode select'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                itemCount: ThemeMode.values.length,
+                itemBuilder: (_, index) {
+                  final themeMode = ThemeMode.values[index];
+                  return RadioListTile<ThemeMode>(
+                    activeColor: ColorName.primary,
+                    value: themeMode, // ラジオボタンのvalue(ThemeModeのenum)
+                    groupValue: currentThemeMode, // 現在選択されているボタン
+                    onChanged: (newTheme) {
+                      themeSelector.changeAndSave(newTheme!);
+                    },
+
+                    title: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Icon(themeMode.iconData),
+                    ),
+                    subtitle: Text(themeMode.subtitle),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:github_repo_search/utils/logger.dart';
+import 'package:github_repo_search/utils/provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Logger.configure();
 
-  runApp(const ProviderScope(child: App()));
+  runApp(
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        )
+      ],
+      child: const App(),
+    ),
+  );
 }
