37a9f39cafad7aac599a1381cae832bbaee6a6fa
森田陸斗 <mrmrt1121@gmail.com>
2022-09-05 13:42:43 +0900

    不要クラスの削除

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:github_repo_search/core/model/github_repos_state.dart';
 import 'package:github_repo_search/core/res/app_theme.dart';
-import 'package:github_repo_search/core/services/api/repo_search_client.dart';
 import 'package:github_repo_search/feature/navigation/navigation_page.dart';
 import 'package:github_repo_search/feature/setting/theme_controller.dart';
 import 'package:github_repo_search/i18n/translations.g.dart';
-import 'package:github_repo_search/utils/logger.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class App extends ConsumerWidget {
@@ -27,32 +24,6 @@ class App extends ConsumerWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       home: const NavigationPage(),
-    );
-  }
-}
-
-class MyHomePage extends ConsumerWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('GitHub'),
-      ),
-      body: const Center(
-        child: Text('Let\'s start the program!'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final items = await ref.watch(repoSearchClientProvider).get(
-            queryParameters: <String, dynamic>{'q': 'YUMEMI'},
-            fromJson: GithubReposState.fromJson,
-          );
-          logger.info(items);
-        },
-        child: const Icon(Icons.search),
-      ),
     );
   }
 }
