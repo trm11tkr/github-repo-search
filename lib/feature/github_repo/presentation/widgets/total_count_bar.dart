cbab2d60d878bbbb0a9a831b4d8a73668d07f6f7
森田陸斗 <mrmrt1121@gmail.com>
2022-09-05 14:19:26 +0900

    TotalCountbarWidgetを作成

---
+++ lib/feature/github_repo/presentation/widgets/total_count_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final totalCountProvider = StateProvider<int?>((_) => null);
+
+class TotalCountBar extends ConsumerWidget {
+  const TotalCountBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final totalCount = ref.watch(totalCountProvider);
+    return SizedBox(
+      width: double.infinity,
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: totalCount != null ? Text('合計$totalCount件') : null,
+      ),
+    );
+  }
+}
