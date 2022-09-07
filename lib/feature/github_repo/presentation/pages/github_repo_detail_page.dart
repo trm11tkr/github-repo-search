f43178ba1078cc2285f669b69f10387c7882d98b
森田陸斗 <mrmrt1121@gmail.com>
2022-09-07 20:30:22 +0900

    詳細ページをGridスタイルに変更

--- lib/feature/github_repo/presentation/pages/github_repo_detail_page.dart
+++ lib/feature/github_repo/presentation/pages/github_repo_detail_page.dart
@@ -14,6 +14,51 @@ class GithubRepoDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final listMap = [
+      _CustomTile(
+        icon: Icons.language,
+        title: i18n.language,
+        iconColor: Colors.blue,
+        child: repository.language != null
+            ? Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.fiber_manual_record,
+                    color: LanguageColors.getLanguageColor(
+                      repository.language,
+                    ),
+                  ),
+                  Text(repository.language!),
+                ],
+              )
+            : const Text('-'),
+      ),
+      _CustomTile(
+        icon: Icons.star_border,
+        title: i18n.star,
+        iconColor: const Color(0xFFedb918),
+        child: Text(repository.stargazersCount.formatComma),
+      ),
+      _CustomTile(
+        icon: Icons.remove_red_eye_outlined,
+        title: i18n.watch,
+        iconColor: Colors.red,
+        child: Text(repository.watchersCount.formatComma),
+      ),
+      _CustomTile(
+        icon: Icons.fork_left,
+        title: i18n.fork,
+        iconColor: Colors.grey,
+        child: Text(repository.forksCount.formatComma),
+      ),
+      _CustomTile(
+        icon: Icons.adjust,
+        title: i18n.issue,
+        iconColor: Colors.green,
+        child: Text(repository.openIssuesCount.formatComma),
+      ),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -24,90 +69,53 @@ class GithubRepoDetailPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(12),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleThumbnail(
+          child: CustomScrollView(
+            slivers: [
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  Align(
+                    child: CircleThumbnail(
+                      size: 150,
                       url: repository.owner.avatarUrl,
                     ),
-                    const Gap(10),
-                    Text(repository.owner.login),
-                  ],
-                ),
-                Text(
-                  repository.name,
-                  style: context.titleStyle,
-                ),
-                const Gap(10),
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  height: 100,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
                   ),
-                  child: SingleChildScrollView(
-                    child: Text(
-                      repository.description,
-                      style: context.subTitleStyle,
+                  const Gap(10),
+                  Center(child: Text(repository.owner.login)),
+                  const Gap(30),
+                  Text(
+                    'リポジトリ：${repository.name}',
+                    style: context.titleStyle,
+                  ),
+                  const Gap(10),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    height: 100,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Text(
+                        repository.description,
+                        style: context.subTitleStyle,
+                      ),
                     ),
                   ),
-                ),
-                _CustomTile(
-                  icon: Icons.language,
-                  title: i18n.language,
-                  iconColor: Colors.blue,
-                  child: repository.language != null
-                      ? Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.fiber_manual_record,
-                              color: context.isDark
-                                  ? LanguageColors.getLanguageColor(
-                                      repository.language,
-                                    )
-                                  : LanguageColors.getLanguageColor(
-                                      repository.language,
-                                    ).withOpacity(0.8),
-                            ),
-                            Text(repository.language!),
-                          ],
-                        )
-                      : const Text('-'),
-                ),
-                _CustomTile(
-                  icon: Icons.star_border,
-                  title: i18n.star,
-                  iconColor: const Color(0xFFedb918),
-                  child: Text(repository.stargazersCount.formatComma),
-                ),
-                _CustomTile(
-                  icon: Icons.remove_red_eye_outlined,
-                  title: i18n.watch,
-                  iconColor: Colors.red,
-                  child: Text(repository.watchersCount.formatComma),
-                ),
-                _CustomTile(
-                  icon: Icons.fork_left,
-                  title: i18n.fork,
-                  iconColor: Colors.grey,
-                  child: Text(repository.forksCount.formatComma),
-                ),
-                _CustomTile(
-                  icon: Icons.adjust,
-                  title: i18n.issue,
-                  iconColor: Colors.green,
-                  child: Text(repository.openIssuesCount.formatComma),
-                ),
-              ],
-            ),
+                  const Gap(20)
+                ]),
+              ),
+              SliverGrid.count(
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 2,
+                crossAxisCount: context.isLandscape ? 5 : 2,
+                children: listMap,
+              )
+            ],
           ),
         ),
       ),
@@ -130,18 +138,29 @@ class _CustomTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ListTile(
-          leading: Icon(
-            icon,
-            color: context.isDark ? iconColor.withOpacity(0.8) : iconColor,
-          ),
-          title: Text(title),
-          trailing: child,
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.white70,
+        border: Border.all(
+          color: context.subTitleStyle.color!,
         ),
-        const Divider(),
-      ],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, color: iconColor),
+              const Gap(5),
+              Text(title),
+            ],
+          ),
+          const Divider(),
+          child,
+        ],
+      ),
     );
   }
 }
