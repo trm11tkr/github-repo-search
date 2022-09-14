67ff6100ff3fd93c69207ce725119ad20a89b51e
森田陸斗 <mrmrt1121@gmail.com>
2022-09-14 19:06:25 +0900

    totalCountProviderでpageProviderをwatchし,値の更新

--- lib/feature/github_repo/presentation/widgets/total_count_bar.dart
+++ lib/feature/github_repo/presentation/widgets/total_count_bar.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_repo_search/core/extension/num_extension.dart';
+import 'package:github_repo_search/feature/github_repo/pagination/pagination_notifier.dart';
 import 'package:github_repo_search/i18n/translations.g.dart';
 
-/// トータルカウントプロバイダー
-final totalCountProvider = StateProvider<int?>((_) => null);
+/// レポジトリ合計数プロバイダー
+
+// pageProviderからの取得データによって更新
+// 初回エラー、初回ローディング時はnullを返却
+// ページング中エラー、ページング中ローディング時は前回値を返却
+final totalCountProvider = StateProvider<int?>((ref) {
+  final totalCount = ref.watch(pageProvider).whenOrNull(
+        data: (data) => data.totalCount,
+        // 前回値を渡す
+        onGoingError: (previousData, e, stk) => previousData.totalCount,
+        // 前回値を渡す
+        onGoingLoading: (previousData) => previousData.totalCount,
+      );
+
+  return totalCount;
+});
 
 class TotalCountBar extends ConsumerWidget {
   const TotalCountBar({super.key});
@@ -15,8 +30,8 @@ class TotalCountBar extends ConsumerWidget {
     return SizedBox(
       width: double.infinity,
       child: Align(
-        /// null(エラー時)は何も表示しない
         alignment: Alignment.centerRight,
+        // エラー時にはnullが渡る(表示しない)
         child: totalCount != null
             ? Text(i18n.totalRepo(totalCount: totalCount.formatComma))
             : null,
